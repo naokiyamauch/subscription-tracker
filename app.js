205070776697aa726e68433e4eb15a870d6bf98b
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-06-10 21:18:29 -0700

    create express server

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.get('/', (req, res) => {
+	res.send('Welcome to the Subscription Tracker API!');
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000, () => {
+	console.log('Subscription Tracker API is running on http://localhost:3000');
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+export default app;
