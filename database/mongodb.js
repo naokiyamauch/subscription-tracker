f09470a3a9af54e61943bea7585edb9e95ad7a52
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-06-13 00:04:38 -0700

    chore: minor fix

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import { DB_URI } from '../config/env.js';
+import { DB_URI, NODE_ENV } from '../config/env.js';
 
 if (!DB_URI) {
 	throw new Error(
@@ -10,8 +10,12 @@ if (!DB_URI) {
 const connectToDatabase = async () => {
 	try {
 		await mongoose.connect(DB_URI);
+
+		console.log(`Connected to database in ${NODE_ENV} mode`);
 	} catch (error) {
 		console.log('Error connecting to database: ', error);
 		process.exit(1);
 	}
 };
+
+export default connectToDatabase;
