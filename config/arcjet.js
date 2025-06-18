a2343bdfb59aca87df30d90d6c38f0052631c0cf
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-06-17 19:01:03 -0700

    Implement arcjet for bot detecting and rate limiting

---
+++ config/arcjet.js
@@ -0,0 +1,24 @@
+import arcjet, { detectBot, shield, tokenBucket } from '@arcjet/node';
+import { ARCJET_KEY } from './env.js';
+
+const aj = arcjet({
+	key: ARCJET_KEY,
+	characteristics: ['ip.src'], // Track requests by IP
+	rules: [
+		shield({ mode: 'LIVE' }),
+
+		detectBot({
+			mode: 'LIVE', // Blocks requests. Use "DRY_RUN" to log only
+			// Block all bots except the following
+			allow: ['CATEGORY:SEARCH_ENGINE'],
+		}),
+		tokenBucket({
+			mode: 'LIVE',
+			refillRate: 5, // Refill 5 tokens per interval
+			interval: 10, // Refill every 10 seconds
+			capacity: 10, // Bucket capacity of 10 tokens
+		}),
+	],
+});
+
+export default aj;
